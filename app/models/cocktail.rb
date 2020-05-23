094fff8dc8ef5c459b703bf941cbb985c6f4ea09
github username <github email address>
2020-05-23 11:11:58 -0400

    Added ability to select cocktail by number and letter

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,39 @@ class Cocktail < ApplicationRecord
     has_many :cocktailIngredients
     has_many :ingredients, through: :cocktailIngredients
     belongs_to :creator, class_name: 'User', foreign_key: 'creator_id', optional: true
+
+    def self.get_cocktails(first_character)
+        cocktails = []
+
+        cocktails += Cocktail.all.select { |c| c.name.first === first_character }
+
+        apiResponse = RestClient.get "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{first_character}"
+
+        if apiResponse
+            apiCocktails = JSON.parse(apiResponse)['drinks']
+            if apiCocktails
+                apiCocktails.each { |cocktail| cocktails.push(parseApiCocktail(cocktail)) }
+            end
+        end
+
+        cocktails
+    end
+
+    def self.parseApiCocktail(apiCocktail)
+        cocktail = Cocktail.new
+        ingredients = []
+        cocktail.name = apiCocktail['strDrink']
+        cocktail.image = apiCocktail['strDrinkThumb']
+
+        15.times do |index|
+            if !apiCocktail["strIngredient#{index + 1}"]
+                break
+            else
+                ingredient = Ingredient.new(name: apiCocktail["strIngredient#{index + 1}"])
+                cocktail.ingredients << ingredient
+            end
+        end
+
+        cocktail
+    end
 end
