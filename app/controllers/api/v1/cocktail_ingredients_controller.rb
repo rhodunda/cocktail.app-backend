e7c32463d70ab72195854292434864621fb15521
github username <github email address>
2020-05-28 01:00:03 -0400

    Added delete ingredient functionality

--- app/controllers/api/v1/cocktail_ingredients_controller.rb
+++ app/controllers/api/v1/cocktail_ingredients_controller.rb
@@ -1,13 +1,7 @@
 class Api::V1::CocktailIngredientsController < ApplicationController
     def index
-<<<<<<< HEAD
-        cocktails = Cocktail.all
-        render json: cocktails
-        
-=======
         cocktail_ingredients = CocktailIngredient.all
         render json: cocktail_ingredients.as_json(include: [:cocktail, :ingredient])
->>>>>>> 19715e221504569a521c3af293ffce768df50d64
     end 
 
     def show
@@ -19,5 +13,14 @@ class Api::V1::CocktailIngredientsController < ApplicationController
         cocktail = Cocktail.new(name: params['name'], image_url: params['image_url'])
         cocktail.save
         render json: cocktail
-    end 
+    end
+
+    def destroy
+        cocktail_ingredient = CocktailIngredient.find(params[:id])
+        if cocktail_ingredient.destroy
+            render json: true
+        else
+            render json: false
+        end
+    end
 end
